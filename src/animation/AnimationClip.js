0ffc8cbd41fab1a4c3b11351a3f5c51b894ee751
liaowei <l.w.kampfer@gmail.com>
2020-01-27 16:21:26 +0800

    完善animation clip和track

--- src/animation/AnimationClip.js
+++ src/animation/AnimationClip.js
@@ -1,3 +1,30 @@
+let uid = 0;
+
 export default class AnimationClip {
     
+    constructor(name, duration = -1, tracks) {
+        this._name = name;
+        this._duration = duration;
+        this._tracks = tracks;
+        this.uuid = ++uid;
+
+        if (this._duration < 0) {
+            this.setDurationFromTracks();
+        }
+    }
+
+    setDurationFromTracks() {
+        let tracks = this._tracks,
+            duration = 0;
+
+        for(let i = 0, l = tracks.length; i < l; i++) {
+            let track = tracks[i];
+            duration = Math.max(duration, track.times[track.times.length - 1]);
+        }
+
+        this._duration = duration;
+
+        return this;
+    }
+
 }
