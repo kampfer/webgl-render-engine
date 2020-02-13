5101e4f55b9f481c26685dc48b2ea5e1a8840486
liaowei <l.w.kampfer@gmail.com>
2020-02-13 11:07:44 +0800

    修复BoxGeometry的问题

--- examples/wireframe.js
+++ examples/wireframe.js
@@ -12,15 +12,26 @@ let geometry = new BoxGeometry(2, 2, 2),
     mesh = new Mesh(geometry, material),
     scene = new Scene(),
     renderer = new WebGLRenderer(),
-    camera = new PerspectiveCamera(60, 1, 0.1, 100);
+    camera = new PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 material.color = [0.5, 0.4, 0.3, 1];
+
+camera.lookAt(0, 0, 0);
+camera.position.set(0.1, 0.2, 10);
+camera.updateWorldMatrix();
 
 scene.add(mesh);
 
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor([0.1, 0.1, 0.1, 1]);
+renderer.setClearColor([1, 1, 1, 1]);
 
 document.body.appendChild(renderer.domElement);
 
-renderer.render(scene, camera);
+function animate() {
+    requestAnimationFrame(animate);
+    mesh.rotation.y += 0.01;
+    mesh.rotation.x += 0.01;
+    renderer.render(scene, camera);
+}
+
+animate();
