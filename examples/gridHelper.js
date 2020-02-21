9843ccc4b49dbd7b259a2e5a9b93bb7ef5b8c2ca
liaowei <l.w.kampfer@gmail.com>
2020-02-21 16:40:12 +0800

    1、完善math/Color 2、增加GridHelper

---
+++ examples/gridHelper.js
@@ -0,0 +1,53 @@
+import GridHelper from '../src/helpers/GridHelper';
+import WebGLRenderer from '../src/renderers/WebGLRenderer';
+import Scene from '../src/objects/Scene';
+import PerspectiveCamera from '../src/cameras/PerspectiveCamera';
+import OrbitController from '../src/controllers/OrbitController';
+
+class Example {
+
+    constructor() {
+        this.scene = new Scene();
+
+        let renderer = new WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.setClearColor([1, 1, 1, 1]);
+        this.renderer = renderer;
+        
+        let camera = new PerspectiveCamera(60 * Math.PI / 180, window.innerWidth / window.innerHeight, 0.1, 100);
+        camera.lookAt(0, 0, 0);
+        camera.position.set(0, 0, 10);
+        camera.updateWorldMatrix();
+        this.camera = camera;
+
+        this.cameraController = new OrbitController(camera, renderer.domElement);
+
+        this.animate = this.animate.bind(this);
+    }
+
+    animate() {
+        this._animationTimer = requestAnimationFrame(this.animate);
+        this.renderer.render(this.scene, this.camera);
+    }
+
+    run() {
+        this.animate();
+    }
+
+    destroy() {
+        this.cameraController.destroy();
+        cancelAnimationFrame(this._animationTimer);
+    }
+
+}
+
+export default class GridHelperExample extends Example {
+
+    constructor() {
+        super();
+
+        let gridHelper = new GridHelper(10, 10);
+        this.scene.add(gridHelper);
+    }
+
+}
---
+++ src/helpers/GridHelper.js
@@ -0,0 +1,31 @@
+// import GraphObject from '../objects/GraphObject';
+import LineSegments from '../objects/LineSegments';
+import BufferAttribute from '../renderers/BufferAttribute';
+import Geometry from '../geometries/Geometry';
+import Material from '../materials/Material';
+import Color from '../math/Color';
+
+export default class GridHelper extends LineSegments {
+
+    constructor(size, divisions) {
+
+        let halfSize = size / 2,
+            step = size / divisions,
+            vertices = [];
+
+        for(let i = 0, k = -halfSize; i < divisions; i++, k += step) {
+            vertices.push(-halfSize, 0, k, halfSize, 0, k);
+            vertices.push(k, 0, -halfSize, k, 0, halfSize);
+        }
+
+        let geometry = new Geometry();
+        geometry.setAttribute('position', new BufferAttribute(new Float32Array(vertices), 3));
+
+        let material = new Material();
+        material.color = new Color('#ccc');
+
+        super(geometry, material);
+
+    }
+
+}
