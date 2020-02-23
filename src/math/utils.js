cb7c539a5308e11ab4a945d3c4ee318824372078
liaowei <l.w.kampfer@gmail.com>
2020-02-23 15:15:25 +0800

    增加camera helper

--- src/math/utils.js
+++ src/math/utils.js
@@ -1,3 +1,14 @@
+const DEG2RAD = Math.PI / 180;
+const RAD2DEG = 180 / Math.PI;
+
 export function clamp(v, min, max) {
     return Math.min(max, Math.max(min, v));
+}
+
+export function degToRad( degrees ) {
+    return degrees * DEG2RAD;
+}
+
+export function radToDeg(radians) {
+    return radians * RAD2DEG;
 }
