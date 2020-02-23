b4c0a1bc718542f60c8c8466236c55d438f1fefd
liaowei <l.w.kampfer@gmail.com>
2020-02-23 16:36:34 +0800

    开发CameraHelper

---
+++ src/helpers/CameraHelper.js
@@ -0,0 +1,12 @@
+import LineSegments from '../objects/LineSegments';
+import BufferAttribute from '../renderers/BufferAttribute';
+
+export default class CameraHelper extends LineSegments {
+
+    constructor(camera) {
+        super();
+
+        
+    }
+
+}
