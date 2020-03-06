327dcce35df94733f86c4ba1390025d807ac9d67
liaowei <l.w.kampfer@gmail.com>
2020-03-06 15:51:33 +0800

    修改npm dev命令

--- scripts/webpack.dev.config.js
+++ scripts/webpack.dev.config.js
@@ -1,60 +1,15 @@
 const config = require('./webpack.config');
 const path = require('path');
-const fs = require('fs');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-let findEntries = function(dir) {
-    let paths = fs.readdirSync(dir),
-        entries = {};
-
-    for(let i = 0, l = paths.length; i < l; i++) {
-        let p = path.join(dir, paths[i]),
-            extname = path.extname(p);
-        if (extname === '' && fs.statSync(p).isDirectory()) {
-            let subEntries = findEntries(p);
-            for (let name in subEntries) {
-                entries[name] = subEntries[name];
-            }
-        } else if(extname === '.html') {
-            let basename = path.basename(p, extname);
-            entries[basename] = {
-                name: basename,
-                html: path.format({
-                    dir,
-                    name: basename,
-                    ext: '.html'
-                }),
-                js: path.format({
-                    dir,
-                    name: basename,
-                    ext: '.js'
-                })
-            };
-        }
-    }
-
-    return entries;
-}
 
 let devConfig = Object.assign(config, {
     mode: 'development',
-    devServer: {
-        contentBase: './dist'
-    },
-    entry: {}
+    entry: path.join(__dirname, '../src/index.js'),
+    output: {
+        filename: 'webglRenderEngine.js',
+        path: path.join(__dirname, '../build'),
+        library: 'webglRenderEngine',
+        libraryTarget: 'umd'
+    }
 });
-
-let entries = findEntries(path.join(__dirname, '../examples'));
-for (let name in entries) {
-    let entry = entries[name];
-    devConfig.entry[name] = entry.js;
-    devConfig.plugins.push(
-        new HtmlWebpackPlugin({
-            filename: name + '.html',
-            template: entry.html,
-            chunks: [name]
-        })
-    );
-}
 
 module.exports = devConfig;
