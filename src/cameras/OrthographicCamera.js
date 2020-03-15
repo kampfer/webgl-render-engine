e62f8bc4e6c493a9e5b87fca9a5f15ff20a05676
liaowei <l.w.kampfer@gmail.com>
2020-03-15 18:49:52 +0800

    orthographic camera支持zoom

--- src/cameras/OrthographicCamera.js
+++ src/cameras/OrthographicCamera.js
@@ -18,8 +18,28 @@ export default class OrthographicCamera extends Camera {
         this.updateProjectionMatrix();
     }
 
+    setZoom(zoom = 1) {
+        this._zoom = zoom;
+        this.updateProjectionMatrix();
+    }
+
     updateProjectionMatrix() {
-        this.projectionMatrix.setOrthographic(this._left, this._right, this._top, this._bottom, this._near, this._far);
+        let dx = ( this._right - this._left ) / ( 2 * this._zoom ),
+            dy = ( this._top - this._bottom ) / ( 2 * this._zoom ),
+            cx = ( this._right + this._left ) / 2,
+            cy = ( this._top + this._bottom ) / 2,
+            left = cx - dx,
+            right = cx + dx,
+            top = cy + dy,
+            bottom = cy - dy;
+
+        // 经过上面的变换之后，left、top、right、bottom转入setOrthographic后计算的结果正好相当于变换矩阵乘以缩放矩阵的结果
+        // left = [right(zoom - 1) + left(zoom + 1)] / (2 * zoom)
+        // right = (right(zoom + 1) + left(zoom - 1)) / (2 * zoom);
+        // right - left = right - left / zoom
+        // right + left = right + left
+
+        this.projectionMatrix.setOrthographic(left, right, top, bottom, this._near, this._far);
         this.projectionMatrixInverse.setInverseOf(this.projectionMatrix);
     }
 
