6b464c49e2d1300b5164a5a698d1bebfb0791840
liaowei <l.w.kampfer@gmail.com>
2020-04-24 15:14:08 +0800

    更新skinned mesh示例

--- examples/skinnedMesh.js
+++ examples/skinnedMesh.js
@@ -4,28 +4,53 @@ import Skeleton from '../src/objects/Skeleton';
 import Bone from '../src/objects/Bone';
 import BoxGeometry from '../src/geometries/BoxGeometry';
 import Material from '../src/materials/Material';
+import Mat4 from '../src/math/Mat4';
+import BufferAttribute from '../src/renderers/BufferAttribute';
+import GridHelper from '../src/helpers/GridHelper';
 
 // https://webglfundamentals.org/webgl/lessons/webgl-skinning.html
 export default class SkinnedMeshExample extends Example {
 
     constructor() {
 
-        super({ useOrbit: false });
+        super();
 
-        let bone = new Bone(),
+        let bone = new Bone(), 
             bones = [bone],
             skeleton = new Skeleton(bones),
             geometry = new BoxGeometry(1, 1, 1),
             material = new Material(),
-            mesh = new SkinnedMesh(geometry, material);
+            mesh = new SkinnedMesh(geometry, material),
+            count = geometry.getAttribute('position').count,
+            skinIndex = new BufferAttribute(new Float32Array(count * 4), 4),
+            skinWeight = new BufferAttribute(new Float32Array(count * 4), 4),
+            bindMatrix = new Mat4();
 
-        mesh.bind(skeleton);
+        geometry.setAttribute('skinIndex', skinIndex);
+        geometry.setAttribute('skinWeight', skinWeight);
 
-        // 将bone加入GraphObject的阶层结构，每次render时才能自动更新worldMatrix
-        // 否则需要手动调用updateWorldMatrix方法
-        mesh.add(bone);
+        for(let i = 0, l = count; i < l; i++) {
 
-        this.scene.add(mesh);
+            skinIndex.setXYZW(i, 0, 0, 0, 0);
+            skinWeight.setXYZW(i, 1, 0, 0, 0);
+
+        }
+
+        // bindMatrix.translate(0, 0, 2);
+
+        mesh.position.x = 2;
+        // mesh.rotation.set(0, 0.5, 0);
+
+        // bone.position.z = 1;
+
+        mesh.bind(skeleton, bindMatrix);
+
+        mesh.bindMode = SkinnedMesh.DETACHED_MODE;
+
+        this.mesh = mesh;
+        this.bone = bone;
+
+        this.gridHelper = new GridHelper(100, 100);
 
         this.renderer.autoClear = false;
 
@@ -33,28 +58,20 @@ export default class SkinnedMeshExample extends Example {
 
     update() {
 
-        let mesh = this.scene.children[0],
-            bone = mesh.skeleton.bones[0];
+        let bone = this.bone;
 
-        bone.rotation.x += 0.01;
+        bone.rotation.y += 0.05;
+        // bone.rotation.y = 3;
+        // bone没有加入节点的阶层结构，所以需要手动更新
+        bone.updateWorldMatrix();
 
     }
 
     render() {
 
-        let mesh = this.scene.children[0];
-
         this.renderer.clear();
-
-        mesh.position.x = 0;
-        mesh.rotation.z = 0;
-
-        this.renderer.render(mesh, this.camera);
-
-        mesh.position.x = 1;
-        mesh.rotation.z = Math.PI / 3;
-
-        this.renderer.render(mesh, this.camera);
+        this.renderer.render(this.gridHelper, this.camera);
+        this.renderer.render(this.mesh, this.camera);
 
     }
 
