e3d8deed9cdefd14589e1fcf087b189ee6660625
liaowei <l.w.kampfer@gmail.com>
2020-08-03 22:33:33 +0800

    update

--- examples/canvasTexture.js
+++ examples/canvasTexture.js
@@ -12,15 +12,18 @@ export default class canvasTextureExample extends Example {
         super();
 
         const offscreen = new OffscreenCanvas(256, 256);
-        offscreen.width = 256;
-        offscreen.height = 256;
-
         const ctx = offscreen.getContext('2d');
-        ctx.font = `100px monospace`;
-        ctx.textAlign = "center" ;
+
+        const text = `${new Date().getTime()}`;
+        const textRect = ctx.measureText(text);
+        const textHeight = 20;
+        const textWidth = textRect.width;
+
+        ctx.font = `20px monospace`;
+        ctx.textAlign = "left" ;
         ctx.textBaseline = "top";
-        ctx.fillStyle = 'red';
-        ctx.fillText('Hi, there!', 0, 0);
+        ctx.fillStyle = 'black';
+        ctx.fillText(text, 0, 0);
 
         const texture = new CanvasTexture({ image: offscreen });
         const geometry = new PlaneGeometry(2, 2);
@@ -31,15 +34,24 @@ export default class canvasTextureExample extends Example {
 
         geometry.setAttribute('uv', new BufferAttribute(
             new Float32Array([
-                1, 1,
+                textWidth / 256, 1,
                 0, 1,
-                0, 0,
-                1, 0
+                0, 1 - (textHeight / 256),
+                textWidth / 256, 1 - (textHeight / 256)
             ]),
             2
         ));
 
         this.scene.add(mesh);
+
+        document.body.appendChild(offscreen);
+
+        this.update = function () {
+
+            
+
+
+        }
 
     }
 
