6952167f52989f9763a3007adce834c2bd8c73a1
liaowei <l.w.kampfer@gmail.com>
2020-08-08 16:52:54 +0800

    优化examples/canvasTexture

--- examples/canvasTexture.js
+++ examples/canvasTexture.js
@@ -1,12 +1,17 @@
 import Example from './Example';
 import CanvasTexture from '../src/textures/CanvasTexture';
-import PlaneGeometry from '../src/geometries/PlaneGeometry';
 import Material2D from '../src/materials/Material2D';
 import Mesh from '../src/objects/Mesh';
 import BufferAttribute from '../src/renderers/BufferAttribute';
 import OrthographicCamera from '../src/cameras/OrthographicCamera';
-import Material from '../src/materials/Material';
 import Color from '../src/math/Color';
+import Geometry from '../src/geometries/Geometry';
+import Vec3 from '../src/math/Vec3';
+
+const x = 100;
+const y = 100;
+const width = 128;
+const height = 128;
 
 export default class canvasTextureExample extends Example {
 
@@ -23,48 +28,98 @@ export default class canvasTextureExample extends Example {
             useOrbit: false
         });
 
-        const offscreen = new OffscreenCanvas(256, 256);
-        const ctx = offscreen.getContext('2d');
-
-        const text = `${new Date().getTime()}`;
-        const textRect = ctx.measureText(text);
-        const textHeight = 20;
-        const textWidth = textRect.width;
-
-        ctx.font = `20px monospace`;
-        ctx.textAlign = "left" ;
-        ctx.textBaseline = "top";
-        ctx.fillStyle = 'black';
-        ctx.fillText(text, 0, 0);
-
-        const texture = new CanvasTexture({ image: offscreen });
-        const geometry = new PlaneGeometry(256 / window.innerWidth, 256 / window.innerHeight);
-        const material = new Material2D();
-        const mesh = new Mesh(geometry, material);
-
-        const rect = new Mesh(geometry, new Material({ color: new Color('red') }));
+        const rect = new Mesh(
+            this.makeGeometry(x, y, width, height),
+            new Material2D({ color: new Color('red') })
+        );
         this.scene.add(rect);
 
-        material.map = texture;
-
+        const geometry = this.makeGeometry(x, y, width, height);
         geometry.setAttribute('uv', new BufferAttribute(
+            // new Float32Array([
+            //     0, 100 / 64,
+            //     100 / 64, 100 / 64,
+            //     0, 0,
+            //     100 / 64, 0
+            // ]),
             new Float32Array([
-                textWidth / 256, 1,
                 0, 1,
-                0, 1 - (textHeight / 256),
-                textWidth / 256, 1 - (textHeight / 256)
+                1, 1,
+                0, 0,
+                1, 0
             ]),
             2
         ));
 
+        const material = new Material2D();
+        const texture = this.makeCanvasTexture();
+        material.map = texture;
+
+        const mesh = new Mesh(geometry, material);
         this.scene.add(mesh);
 
         this.update = function () {
-
-            
-
-
+            const text = `${new Date().getTime()}`;
+            const ctx = texture.image.getContext('2d');
+            ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+            ctx.fillText(text, 0, 0);
         }
+
+    }
+
+    makeVertexFromPixel(x, y) {
+        const halfWidth = window.innerWidth / 2;
+        const halfHeight = window.innerHeight / 2;
+        return new Vec3((x - halfWidth) / halfWidth, (halfHeight - y) / halfHeight, 0);
+    }
+
+    /**
+     * 0 - 1
+     * |   |
+     * 2 - 3
+     */
+    makeGeometry(x, y, width, height) {
+
+        const camera = this.camera;
+        const vertex1 = this.makeVertexFromPixel(x, y).unproject(camera);
+        const vertex2 = this.makeVertexFromPixel(x + width, y).unproject(camera);
+        const vertex3 = this.makeVertexFromPixel(x, y + height).unproject(camera);
+        const vertex4 = this.makeVertexFromPixel(x + width, y + height).unproject(camera);
+        const vertices = [
+            vertex1.x, vertex1.y, vertex1.z,
+            vertex2.x, vertex2.y, vertex2.z,
+            vertex3.x, vertex3.y, vertex3.z,
+            vertex4.x, vertex4.y, vertex4.z,
+        ];
+
+        const geometry = new Geometry();
+        geometry.setAttribute('position', new BufferAttribute(new Float32Array(vertices), 3));
+        geometry.setIndex(new BufferAttribute(new Uint8Array([0, 2, 3, 0, 3, 1]), 1));
+
+        return geometry;
+
+    }
+
+    makeCanvasTexture() {
+
+        // const offscreen = new OffscreenCanvas(width, height);
+        const offscreen = document.createElement('canvas');
+        offscreen.width = width;
+        offscreen.height = height;
+        document.body.appendChild(offscreen);
+        const ctx = offscreen.getContext('2d');
+
+        ctx.font = `12px monospace`;
+        ctx.textAlign = "left" ;
+        ctx.textBaseline = "top";
+        ctx.fillStyle = 'black';
+
+        const text = `${new Date().getTime()}`;
+        ctx.fillText(text, 0, 0);
+
+        const texture = new CanvasTexture({ image: offscreen });
+
+        return texture;
 
     }
 
