3d2af690ff8b5a631b84148819d2cc692b2fb527
liaowei <l.w.kampfer@gmail.com>
2020-08-14 16:40:47 +0800

    更新example

--- examples/morphTargets.js
+++ examples/morphTargets.js
@@ -9,6 +9,7 @@ import {
     Vec3,
     BufferAttribute,
     NumberKeyFrameTrack,
+    VectorKeyFrameTrack,
 } from '../src';
 
 export default class MorphTargetsExample extends Example {
@@ -26,28 +27,29 @@ export default class MorphTargetsExample extends Example {
             useOrbit: false
         });
 
-        const buffer = this.makeRectangleBuffer(100, 100, 0, 30);
-        const buffer2 = this.makeRectangleBuffer(100, 100, 100, 30);
-        const geometry = new Geometry();
+        const rect1 = this.makeRectangle(100, 100, 100, 30, 'red');
+        const rect2 = this.makeRectangle(100, 150, 200, 30, 'blue');
 
-        geometry.setAttribute('position', buffer);
-        geometry.setMorphAttribute('position', [buffer2]);
-        geometry.setIndex(new BufferAttribute(new Uint8Array([0, 2, 3, 0, 3, 1]), 1));
+        const start = this.pixel2NDC(100, 100).unproject(camera);
+        const end = this.pixel2NDC(100, 150).unproject(camera);
+        const diff = end.sub(start);
 
-        const material = new Material({ color: 'red' });
-        material.morphTargets = true;
-
-        const mesh = new Mesh(geometry, material);
-
-        this.scene.add(mesh);
+        this.scene.add(rect1);
+        this.scene.add(rect2);
 
         this.mixer = new AnimationMixer([
             new AnimationClip('grow', undefined, [
-                new NumberKeyFrameTrack(mesh, 'morphTargetInfluences', [0, 2], [0, 1])
+                new NumberKeyFrameTrack(rect1, 'morphTargetInfluences', [0, 1], [0, 1]),
+                new NumberKeyFrameTrack(rect2, 'morphTargetInfluences', [0, 1], [0, 1])
+            ]),
+            new AnimationClip('change', undefined, [
+                new VectorKeyFrameTrack(rect1, 'position', [0, 0.5, 0.6], [0, 0, 0, diff.x, diff.y, diff.z]),
+                new VectorKeyFrameTrack(rect2, 'position', [0, 0.5, 0.6], [0, 0, 0, -diff.x, -diff.y, -diff.z])
             ])
         ]);
 
         this.mixer.playClip('grow');
+        this.mixer.playClip('change');
 
     }
 
@@ -74,6 +76,23 @@ export default class MorphTargetsExample extends Example {
             vertex4.x, vertex4.y, vertex4.z,
         ];
         return new BufferAttribute(new Float32Array(vertices), 3);
+    }
+
+    makeRectangle(x, y, width, height, color) {
+        const buffer = this.makeRectangleBuffer(x, y, 0, height);
+        const buffer2 = this.makeRectangleBuffer(x, y, width, height);
+        const geometry = new Geometry();
+
+        geometry.setAttribute('position', buffer);
+        geometry.setMorphAttribute('position', [buffer2]);
+        geometry.setIndex(new BufferAttribute(new Uint8Array([0, 2, 3, 0, 3, 1]), 1));
+
+        const material = new Material({ color });
+        material.morphTargets = true;
+
+        const mesh = new Mesh(geometry, material);
+
+        return mesh;
     }
 
 }
